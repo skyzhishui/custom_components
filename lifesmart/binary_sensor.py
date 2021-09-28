ee972647d4dbbb9c0cd2678f9d94b98064c4cbce
skyzhishui <skyzhishui@qq.com>
2021-09-28 14:42:27 +0800

    Update binary_sensor.py

--- lifesmart/binary_sensor.py
+++ lifesmart/binary_sensor.py
@@ -33,6 +33,7 @@ class LifeSmartBinarySensor(LifeSmartDevice, BinarySensorDevice):
 
     def __init__(self, dev, idx, val, param):
         super().__init__(dev, idx, val, param)
+        dev['agt'] = dev['agt'].replace("_","")
         self.entity_id = ENTITY_ID_FORMAT.format(( dev['devtype'] + "_" + dev['agt'] + "_" + dev['me'] + "_" + idx).lower())
         devtype = dev['devtype']
         if devtype in GUARD_SENSOR:
